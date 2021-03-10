beded3989091aa68699779a7fb1741e729fb5fa8
Realdo Dias <realdo@omesti.com>
2021-03-10 13:22:40 +0800

    add navigation withRouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -10,35 +10,35 @@ class Directory extends React.Component {
     this.state = {
       sections: [
         {
+          id: 1,
           title: 'hats',
           imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-          id: 1,
           linkUrl: 'shop/hats'
         },
         {
+          id: 2,
           title: 'jackets',
           imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-          id: 2,
           linkUrl: 'shop/jackets'
         },
         {
+          id: 3,
           title: 'sneakers',
           imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-          id: 3,
           linkUrl: 'shop/sneakers'
         },
         {
+          id: 4,
           title: 'womens',
           imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
           size: 'large',
-          id: 4,
           linkUrl: 'shop/womens'
         },
         {
+          id: 5,
           title: 'mens',
           imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
           size: 'large',
-          id: 5,
           linkUrl: 'shop/mens'
         }
       ]
@@ -49,8 +49,8 @@ class Directory extends React.Component {
     return (
       <div className='directory-menu'>
         {
-          this.state.sections.map(({ id, title, imageUrl, size }) => (
-            <MenuItem id={id} title={title} imageUrl={imageUrl} size={size} />
+          this.state.sections.map(({ id, ...otherSectionsProps }) => (
+            <MenuItem  key={id} {...otherSectionsProps} />
           ))
         }
       </div>
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import { withRouter } from 'react-router-dom';
+
 import './menu-item.styles.scss'
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div style={{ backgroundImage: `url(${imageUrl})` }} className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, history, match, linkUrl }) => (
+  <div 
+    style={{ backgroundImage: `url(${imageUrl})` }} 
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
     <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }} />
     <div className="content">
       <h1 className="title">{title.toUpperCase()}</h1>
@@ -12,4 +18,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
   </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
