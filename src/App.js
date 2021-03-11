c3818f3f6803c9982272bcf5d61410e13ba709e4
Realdo Dias <realdo@omesti.com>
2021-03-11 23:02:15 +0800

    add collections and header components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import { Route, Switch } from 'react-router-dom';
 
-import HomePage from './pages/homepage/homepage.component';
+import HomePage from './pages/home/home.component';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component'
 
 import './App.css';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
